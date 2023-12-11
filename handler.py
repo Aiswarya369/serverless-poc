2c31b29334c5f52f5d7ddf7a24efaaaf9bc3c18f
Aiswarya R <aiswarya.r@experionglobal.com>
2023-12-11 21:28:03 +0530

    add the test script

--- handler.py
+++ handler.py
@@ -1,10 +1,19 @@
+import requests
 import json
 
+url = "https://rrki1p4u90.execute-api.ap-south-1.amazonaws.com/dev/service/lc/load/e67baf7fb8514a378baf08b0b4273856"
+headers = {"Content-Type": "application/json"}
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
-
-    return {"statusCode": 200, "body": json.dumps(body)}
+for i in range(1, 6):
+    payload = json.dumps(
+        {
+            "site": "AJ0000000{}".format(i),
+            "start_datetime": "2023-12-11T16:00:00+00:00",
+            "end_datetime": "2023-12-11T16:30:00+00:00",
+            "status": "ON",
+            "switch_addresses": "LG02210255{}".format(i),
+            "group_id": "g1",
+        }
+    )
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response)
