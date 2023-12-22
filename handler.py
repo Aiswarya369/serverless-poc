180ff4fce94ccec040cfa0b3d3c825400f19fc0a
Aiswarya R <aiswarya.r@experionglobal.com>
2023-12-22 13:27:32 +0530

    roles changes

--- handler.py
+++ handler.py
@@ -1,14 +1,14 @@
 import requests
 import json
 
-url = "https://tpwutkedr3.execute-api.ap-south-1.amazonaws.com/dev/service/lc/load/e67baf7fb8514a378baf08b0b4273856"
+url = "https://njan5dv35a.execute-api.ap-south-1.amazonaws.com/dev/service/lc/load/e67baf7fb8514a378baf08b0b4273856"
 headers = {"Content-Type": "application/json"}
 
-for i in range(3, 4):
+for i in range(7, 8):
     req = {
         "site": "AJ0000000{}".format(i),
-        "start_datetime": "2023-12-21T10:45:00+00:00",
-        "end_datetime": "2023-12-21T11:00:00+00:00",
+        "start_datetime": "2023-12-22T08:15:00+00:00",
+        "end_datetime": "2023-12-22T08:30:00+00:00",
         "status": "ON",
         "switch_addresses": "LG02210255{}".format(i),
         # "group_id": "G4",
