b4a752e466039d340ef30826ca13edef4446c2ee
Aiswarya R <aiswarya.r@experionglobal.com>
2023-12-28 08:49:36 +0530

    changes

--- handler.py
+++ handler.py
@@ -1,19 +1,20 @@
 import requests
 import json
 
-url = "https://5vka5woi90.execute-api.ap-south-1.amazonaws.com/dev/service/lc/load/e67baf7fb8514a378baf08b0b4273856"
+url = "https://rt2tajh0a9.execute-api.ap-south-1.amazonaws.com/dev/service/lc/load/e67baf7fb8514a378baf08b0b4273856"
 headers = {"Content-Type": "application/json"}
 
 for i in range(1, 2):
-    payload = json.dumps(
-        {
-            "site": "CNTEST2000XXXXX",
-            "start_datetime": "2023-12-19T15:30:00+00:00",
-            "end_datetime": "2023-12-19T15:45:00+00:00",
-            "status": "ON",
-            "switch_addresses": "LG021812235",
-        }
-    )
+    req = {
+        "site": "CNTEST2000XXXXX",
+        "start_datetime": "2023-12-23T03:00:00+00:00",
+        "end_datetime": "2023-12-23T04:15:00+00:00",
+        "status": "ON",
+        "switch_addresses": "LG021812235",
+    }
+
+    payload = json.dumps(req)
     response = requests.request("POST", url, headers=headers, data=payload)
-    response_dict = json.loads(response.text)
-    print(response_dict)
+    print(json.dumps(req, indent=4))
+    print(response.status_code, response.reason)
+    print(json.dumps(response.json(), indent=4))
